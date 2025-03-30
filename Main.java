54bb8d3f21dd438722a3b05bb9b3267b6df5db80
vishakh-k <vishakhkt.2003@gmail.com>
2025-03-30 21:03:10 +0530

    ADD second largest number finder program

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    /**
+     * Calculates sum of two integers
+     */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Prints an array elements
+     */
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
---
+++ day1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class day1 {
+     public static int secondLargest(int[] arr){
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max1){
+                max2=max1;
+                max1=arr[i];
+            }else if(arr[i]>max2 && arr[i]!=max1){
+                max2=arr[i];
+            }        
+        }
+        return (max2==Integer.MIN_VALUE)? -1 : max2;
+     }
+     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the size of an array");
+        int n=scanner.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the array elements");
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        int ans = day1.secondLargest(arr);
+        System.out.println("The second largest is:"+ans);
+        scanner.close();
+    }
+}
